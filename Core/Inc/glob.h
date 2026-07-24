126c870d6b13ebd4b94489cd7a7aa17ccb97b507
sho sato <sho.hedwig.s@gmail.com>
2026-07-24 22:12:53 +0900

    人力fix

--- Core/Inc/glob.h
+++ Core/Inc/glob.h
@@ -20,8 +20,8 @@
 /* ăăŻă­ */
 #define FABS(x)						( (x)>=0 ? (x) : -(x) )
 
-#define GOAL_MAP_X_DEF					( 5 )//2
-#define GOAL_MAP_Y_DEF					( 6 )//11
+#define GOAL_MAP_X_DEF					( 7 )//2
+#define GOAL_MAP_Y_DEF					( 7 )//11
 #define MAP_X_SIZE					( 32 )				
 #define MAP_Y_SIZE					( 32 )				
 
@@ -32,16 +32,16 @@
 
 #define VCC_MAX						( 4.2f )							
 #define TIRE_D						( 0.01257f )	
-#define TREAD_IMAGIN					(0.037f)
-#define TREAD						( 0.035f )
+#define TREAD_IMAGIN					(0.0355f)
+#define TREAD						( 0.0342f )
 #define ROTATE_PULSE				( 4096 )									
 #define DIST_1STEP					( PI * TIRE_D / ROTATE_PULSE )			
 #define F_CNT2MM(cnt)				( (float)cnt * DIST_1STEP )				
 #define GEAR_RATIO					(37.0f/8.0f)
 
-#define WEIGHT						(0.0214f)
-#define INERTIA						(1.4f/100000.0f)
-#define TORQUE_CONSTANT				(0.41f/1000.0f)
+#define WEIGHT						(0.0214f)//kg
+#define INERTIA						(10.0f/1000000.0f)//kgm^2
+#define TORQUE_CONSTANT				(0.41f/1000.0f)//Nm/A
 #define MOTOR_REGISTER				(2.3f)
 
 #define DEG_TO_RAD  (3.1416f/180.0f)
